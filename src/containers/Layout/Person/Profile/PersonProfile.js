46dc85290d030ce6a88e3d1d1e5f442040556546
pokiebob <ccursetjee@gmail.com>
2020-12-28 21:35:42 -0500

    basic grid (rating, followers, following), accesses player data from db

--- src/containers/Layout/Person/Profile/PersonProfile.js
+++ src/containers/Layout/Person/Profile/PersonProfile.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    padding: theme.spacing(5),
+    padding: theme.spacing(10),
+  },
+  heading: {
+    color: theme.palette.text.primary,
     textAlign: "center",
+    "font-size": "30px",
+  },
+  subtext: {
     color: theme.palette.text.secondary,
+    textAlign: "center",
+    "font-size": "13px",
   },
   large: {
     width: theme.spacing(15),
     height: theme.spacing(15),
+    margin: "auto"
   },
+  name: {
+    "margin-top": "20px",
+  }
 }));
 
 const getPlayerId = () => {
@@ -25,30 +38,58 @@ const getPlayerId = () => {
 };
 
 const playerPage = () => {
+
+  const [playerState, setPlayerState] = useState(undefined);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/person/1")
+      .then(resp => resp.json())
+      .then((x) => {
+        console.log('within then x:', x);
+        setPlayerState(x);
+      })
+  }, [])
+
+  console.log(playerState);
+
   const classes = useStyles();
 
   const imgSource = `https://randomuser.me/api/portraits/men/${getPlayerId()}.jpg`;
 
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={1}>
-          <Grid item xs={6} sm={3}>
-            <Avatar src={imgSource} className={classes.large} />
+
+  const renderProfile = () => {
+    return (
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Grid container spacing={3}>
+            <Grid item xs={8} sm={4} className={classes.heading}>
+              <Avatar src={imgSource} className={classes.large} />
+              <div className={classes.name}> {playerState.firstName} </div>
+            </Grid>
+            <Grid container xs={8} item >
+              <Grid xs={4} item >
+                <div className={classes.heading}>234</div>
+                <div className={classes.subtext}>Rating</div>
+              </Grid>
+              <Grid xs={4} item >
+                <div className={classes.heading}>343</div>
+                <div className={classes.subtext}>Followers</div>
+              </Grid>
+              <Grid xs={4} item >
+                <div className={classes.heading}>3456</div>
+                <div className={classes.subtext}>Following</div>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={6} sm={3}>
-            col 1
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            col 2
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            col 3
-          </Grid>
-        </Grid>
-      </Paper>
-    </div>
-  );
+        </Paper>
+      </div>
+    );
+  }
+
+
+  if (!playerState) return false
+  else return renderProfile();
+
 };
 
 export default playerPage;
