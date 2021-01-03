5836b58e912cb82f0da11609ca54100a5b7b7067
Cyrus Cursetjee <ccursetjee@gmail.com>
2021-01-02 19:06:34 -0500

    added basic appbar for clubs

--- src/containers/Layout/Person/Profile/PersonProfile.js
+++ src/containers/Layout/Person/Profile/PersonProfile.js
@@ -3,7 +3,55 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import HelpIcon from "@material-ui/icons/Help";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import ThumbDown from "@material-ui/icons/ThumbDown";
+import ThumbUp from "@material-ui/icons/ThumbUp";
+
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={1}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    "aria-controls": `scrollable-force-tabpanel-${index}`
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,6 +60,10 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: "30px",
     width: 800
+  },
+  bar: {
+    border: "none",
+    boxShadow: "none"
   },
   container: {
     marginTop: "20px",
@@ -72,6 +124,12 @@ const playerPage = () => {
 
   console.log(playerState);
 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const classes = useStyles();
 
   const renderProfile = () => {
@@ -110,7 +168,57 @@ const playerPage = () => {
                 <div className={classes.heading} >Clubs</div>
               </Grid>
               <Grid container xs={8} item >
-                <Grid xs={3} item >
+
+              <AppBar position="static" color="white" className={classes.bar}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="on"
+          indicatorColor="primary"
+          textColor="primary"
+          aria-label="scrollable force tabs example"
+        >
+          <Tab
+   label="Item One"
+   {...a11yProps(0)}
+   value={value}
+   component={() => (
+      <Button onClick={() => setValue(0)}> 
+         <Avatar src="<an image>" />
+      </Button>
+   )}
+/>
+          <Tab label="Item Two" icon={<FavoriteIcon />} {...a11yProps(1)} />
+          <Tab label="Item Three" icon={<PersonPinIcon />} {...a11yProps(2)} />
+          <Tab label="Item Four" icon={<HelpIcon />} {...a11yProps(3)} />
+          <Tab label="Item Five" icon={<ShoppingBasket />} {...a11yProps(4)} />
+          <Tab label="Item Six" icon={<ThumbDown />} {...a11yProps(5)} />
+          <Tab label="Item Seven" icon={<ThumbUp />} {...a11yProps(6)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel>
+                {/* <Grid xs={3} item >
                   <div className={classes.heading}>Image</div>
                   <div className={classes.subtext}>Club 1</div>
                 </Grid>
@@ -125,7 +233,7 @@ const playerPage = () => {
                 <Grid xs={3} item >
                   <div className={classes.heading}>Image</div>
                   <div className={classes.subtext}>Club 4</div>
-                </Grid>
+                </Grid> */}
               </Grid>
             </Grid>
 
