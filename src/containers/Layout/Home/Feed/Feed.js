75ac138f4f04fd619080a8cd187529e8ef72d4ff
Cyrus Cursetjee <ccursetjee@gmail.com>
2021-07-31 21:49:56 -0400

    home displays feed page, displays same posts as person profile

--- src/containers/Layout/Home/Feed/Feed.js
+++ src/containers/Layout/Home/Feed/Feed.js
@@ -1,99 +1,134 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { APP_PAPER_ELEVATION } from "../../../../app-config";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper"
+import Paper from "@material-ui/core/Paper";
+import PostingCard from "../../../../components/Card/PostingCard";
+import { Context } from "../../../../Context";
+import { API_URL } from "../../../../api-url";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 0,
-    },
-    paper: {
-      marginTop: "30px",
-      width: "100%",
-      "min-width": "400px",
-      "max-width": "700px"
-    },
-    bar: {
-      border: "none",
-      boxShadow: "none"
-    },
-    container: {
-      marginTop: "20px",
-      marginBottom: "20px",
-    },
-    stats: {
-      color: theme.palette.text.primary,
-      textAlign: "center",
-      "font-size": "30px",
-    },
-    heading: {
-      color: theme.palette.text.primary,
-      textAlign: "center",
-      "font-size": "30px",
-      marginTop: "15px"
-    },
-    subheading: {
-      color: theme.palette.text.primary,
-      textAlign: "center",
-      "font-size": "15px",
-    },
-    subtext: {
-      color: theme.palette.text.secondary,
-      textAlign: "center",
-      "font-size": "13px",
-    },
-    bio: {
-      color: theme.palette.text.secondary,
-      textAlign: "center",
-      "font-size": "15px",
-      marginTop: "20px",
-      marginBottom: "20px"
-    },
-    usattLabel: {
-      color: theme.palette.text.secondary,
-      textAlign: "center",
-      "font-size": "13px",
-      marginTop: "10px",
-    },
-    large: {
-      width: theme.spacing(15),
-      height: theme.spacing(15),
-      margin: "auto"
-    },
-    name: {
-      "margin-top": "20px",
-      "font-size": "18px",
-      textAlign: "center",
-    }
-  }));
+  root: {
+    flexGrow: 0,
+  },
+  paper: {
+    marginTop: "30px",
+    width: "100%",
+    "min-width": "400px",
+    "max-width": "700px"
+  },
+  bar: {
+    border: "none",
+    boxShadow: "none"
+  },
+  container: {
+    marginTop: "20px",
+    marginBottom: "20px",
+  },
+  stats: {
+    color: theme.palette.text.primary,
+    textAlign: "center",
+    "font-size": "30px",
+  },
+  heading: {
+    color: theme.palette.text.primary,
+    textAlign: "center",
+    "font-size": "30px",
+    marginTop: "15px"
+  },
+  subheading: {
+    color: theme.palette.text.primary,
+    textAlign: "center",
+    "font-size": "15px",
+  },
+  subtext: {
+    color: theme.palette.text.secondary,
+    textAlign: "center",
+    "font-size": "13px",
+  },
+  bio: {
+    color: theme.palette.text.secondary,
+    textAlign: "center",
+    "font-size": "15px",
+    marginTop: "20px",
+    marginBottom: "20px"
+  },
+  usattLabel: {
+    color: theme.palette.text.secondary,
+    textAlign: "center",
+    "font-size": "13px",
+    marginTop: "10px",
+  },
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: "auto"
+  },
+  name: {
+    "margin-top": "20px",
+    "font-size": "18px",
+    textAlign: "center",
+  }
+}));
 
 const feed = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    /** API CALL 
-     *  send user ID
-     *  backend filters posts based on likes and user prefernces
-     *  and returns array of posts in response
-    */
-    
+  const [postingState, setPostingState] = useState();
+  const [userContext, setUserContext] = useContext(Context);
 
-    const renderPostings = () => {
-        <Paper className={classes.paper} elevation={APP_PAPER_ELEVATION}>
-            <Grid container className={classes.container} >
-               uh 
+  const initialize = () => {
+    fetch(API_URL.post + "find/?page=pp&pid=" + userContext.personId)
+      .then(resp => resp.json())
+      .then((postings) => {
+        console.log('postings', postings);
+        setPostingState(postings.reverse());
+      });
+  }
+
+  useEffect(() => {
+    initialize();
+  })
+
+  /** API CALL 
+   *  send user ID
+   *  backend filters posts based on likes and user prefernces
+   *  and returns array of posts in response
+  */
+
+
+  const renderPostings = () => {
+    return (
+      postingState?.map((post, idx) => {
+        const date = new Date(post.date);
+        return (
+          <Paper className={classes.paper} elevation={APP_PAPER_ELEVATION}>
+            <Grid container className={classes.container}>
+              <PostingCard
+                pictureUrl={""}
+                name={""}
+                title={post.title}
+                fileUrl={post.fileUrl}
+                fileType={post.fileType}
+                description={post.description}
+                date={date.toLocaleDateString()}
+              />
             </Grid>
-        </Paper>
-    }
-
-
-    return(
-        <div className={classes.root}>
-            <Grid container justify="center" >
-                {renderPostings()}
-            </Grid>
-        </div>
+          </Paper>
+        );
+      }
+      )
     );
+  }
+
+
+  return (
+    <div className={classes.root}>
+      <Grid container justify="center" >
+        {renderPostings()}
+      </Grid>
+    </div>
+  );
 }
 
 export default feed;
